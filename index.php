c818c322f98d5c0cf79e71af1c9f209dafa499bc
GianniPio <giannipiopellegrino00@gmail.com>
2022-01-18 15:29:23 +0100

    new file with comment

---
+++ index.php
@@ -0,0 +1,24 @@
+<!--    
+Definire classe Persona:
+    - ATTRIBUTI (private):
+        - nome
+        - cognome
+        - dataNascita (stringa)
+    - METODI:
+        - costruttore che accetta nome e cognome
+        - setter/getter per ogni variabile
+        - printFullPerson: che stampa "nome cognome: dataNascita"
+        - toString: che ritorna "nome cognome: dataNascita"
+
+
+Definire classe Employee che eredita da Persona:
+    - ATTRIBUTI (private):
+        - stipendio
+        - dataAssunzione
+    - METODI:
+        - costruttore che accetta nome, cognome e stipendio
+        - setter/getter per variabili 
+        - printFullEmployee: che stampa "nome cognome: stipendio (dataAssunzione)"
+        - toString: che ritorna "nome cognome: stipendio (dataAssunzione)" -->
+
+     
